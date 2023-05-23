d73562e092b6b1899e87e1f70de0c24256481851
Yaroslaw Kyrylenko <83594359+gamabuntafrog@users.noreply.github.com>
2023-05-23 20:49:43 +0300

    Add files via upload

---
+++ 21.1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+
+            double[] roots = Discriminant(a, b, c);
+
+            if (roots.Length == 0)
+            {
+                label4.Text = "Коренів немає";
+            }
+            else if (roots.Length == 1)
+            {
+                label4.Text = $"Корінь рівняння: {roots[0]}";
+            }
+            else
+            {
+                label4.Text = $"Корені рівняння: {roots[0]}, {roots[1]}";
+            }
+        }
+        private double[] Discriminant(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+
+                return new double[0];
+            }
+            else if (discriminant == 0)
+            {
+
+                double root = -b / (2 * a);
+                return new double[] { root };
+            }
+            else
+            {
+
+                double sqrtDiscriminant = Math.Sqrt(discriminant);
+                double root1 = (-b + sqrtDiscriminant) / (2 * a);
+                double root2 = (-b - sqrtDiscriminant) / (2 * a);
+                return new double[] { root1, root2 };
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox4.Text);
+            double b = double.Parse(textBox5.Text);
+            double c = double.Parse(textBox6.Text);
+
+            double[] roots = Discriminant(a, b, c);
+
+            if (roots.Length == 0)
+            {
+                label9.Text = "Коренів немає";
+            }
+            else if (roots.Length == 1)
+            {
+                label9.Text = $"Корінь рівняння: {roots[0]}";
+            }
+            else
+            {
+                label9.Text = $"Корені рівняння: {roots[0]}, {roots[1]}";
+            }
+        }
+    }
+}
---
+++ 21.2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+
+            int n = int.Parse(textBox2.Text);
+
+            double result = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                result *= (a + i);
+            }
+            MessageBox.Show($"Результат" + result);
+            label3.Text = "Результат: " + result;
+        }
+    }
+}
